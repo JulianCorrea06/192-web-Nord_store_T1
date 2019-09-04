6108e08fa221b75171109501cbb35b171e3552b0
Julian Correa <geniojuli@gmail.com>
2019-09-04 00:08:50 -0500

    Javascript + Audio

---
+++ main.js
@@ -0,0 +1,17 @@
+var
+
+function handleScroll(event){
+
+        var nav = document.querySelector('.header__menu');
+        var navItems = nav.querySelectorAll('.hypertext');
+
+        for(var i=0; i < navItems.length;i++){
+            var navItem = navItems[i];
+            var selector = navItem.getAttribute('href');
+            var title = document.querySelector(selector);
+            if(window.scrollY > title.offsetTop){
+                
+            }
+        }
+}
+window.addEventListener('scroll', handleScroll);
