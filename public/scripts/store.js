c6a42f5a5fbe73f8e04839962835dd13c4b142fd
Julian Correa <geniojuli@gmail.com>
2019-11-02 17:24:24 -0500

    Página de Tiendaesquematizada - No filtros

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -12,6 +12,74 @@ window.addEventListener('load', function(){
         });
     }
 
-    displayList();
+  var range = document.querySelector('.buffer__slider');
+  var sliderlabel = document.querySelector('.buffer__label');
+  range.addEventListener('input', () =>{
+    sliderlabel.innerHTML = '$' + range.value;
+  });
+    /*
+    function handleLast(){
+
+        tape.classList.add('slider__tape--inactive');
+        
+      }
+      
+      
+      function handleClickr(event) {
+        if(count<4){
+          count++;
+        }
+        
+        if(count ==1){
+          tape.classList.remove('slider__tape--inactive');
+        }
+        
+        var mov = slider.offsetWidth * -0.78 * count;
+        tape.style.transform = 'translate(' + mov + 'px, 0px)';
+      
+        
+        
+      }
+      
+      
+      
+      rbutton.addEventListener('click', handleClickr);
+      
+      function handleClickl(event) {
+        if(count>0){
+          count--;
+        }
+        
+        var mov = slider.offsetWidth * +0.78 * -count;
+        tape.style.transform = 'translate(' + mov + 'px, 0px)';
+        
+        
+      }
+      /*
+      lbutton.addEventListener('click', handleClickl);
+      if (elInput) {
+        var label = document.querySelector('.buffer__label');
+        
+        
+        
+        if (label) {
+          label.innerHTML = elInput.value;
+          
+          elInput.addEventListener('input', function() {
+            label.innerHTML = "$" + elInput.value;
+            aparecer();
+            
+            
+          }, false);
+        }
+        
+      }
+      */
+    
+
+    
+
+
     
  });
+ 
