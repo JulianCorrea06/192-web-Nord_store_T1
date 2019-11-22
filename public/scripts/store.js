ada798da19abd1d2aac0b090aa82128564ec9105
Julian Correa <geniojuli@gmail.com>
2019-11-21 22:08:20 -0500

    Terminando Carrito

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function(){ 
 
   var car = document.querySelectorAll('.kart_btn');
-  car.forEach(product=>{
+  var shopping_counter = document.querySelector('.shopping_counter');
+
+ /* car.forEach(product=>{
 
     product.addEventListener("click", function(){
 
@@ -12,7 +14,41 @@ window.addEventListener('load', function(){
         method: 'POST',
         body : data
     });
+
+    promise.then((raw)=>{
+        return raw.json();
+    }).then(info =>{
+      displayCart();
+      console.log(data);
+    });
   });
+  */
+
+  car.forEach(function(btn) {
+
+    btn.addEventListener('click', function(event){
+        
+        event.preventDefault();
+
+        var data = new URLSearchParams();
+        data.append("idProduct", btn.getAttribute("data-id")); //idProduct es el nombre de la variable
+
+        // console.log("helow madafaka")
+
+        var promise = fetch('/api/kart/', { method: 'POST', body : data });
+
+        promise.then(function(response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function(data) {//data es lo que le mando
+                updateCart(); //aquí se llama la función que refresca el carrito
+                console.log(data);
+               // shopping_counter.innerText = data.cartLength;
+            });
+
+    });
+
 
   var range = document.querySelector('.buffer__slider');
   var sliderlabel = document.querySelector('.buffer__label');
--- routes.js
+++ routes.js
@@ -24,7 +24,7 @@ function createRoutes (app, db) {
             });
     });
 
-    app.get('/kart', (request, response) => {
+   /* app.get('/kart', (request, response) => {
         const car = db.collection('carProducts');
 
         //buscamos todos los productos
@@ -39,6 +39,88 @@ function createRoutes (app, db) {
                 response.render('carProducts',context);
             });
     });
+
+    app.get('/kart', (request, response) => {
+        const car = db.collection('carProducts');
+
+        //buscamos todos los productos
+        car.find({})
+            //transformamos el cursor a una arreglo
+            .toArray((err, result) => {
+                //aseguramos de que no hay errorno
+                assert.equal(null, err);
+
+                var context = {product: result[0]};
+
+                response.render('carProducts',context);
+            });
+    });
+
+    */
+
+   app.get('/api/kart/',(request,response)=>{
+    const cart = db.collection('carProducts');
+
+    cart.find({}).toArray((err, result) => { //result es lo que me trae
+
+        //aseguramos de que no hay error
+        assert.equal(null, err);
+        response.send(result[0]);
+    });
+});
+
+   app.post('/api/kart/',(request,response)=>{
+    const cart = db.collection('carProducts'); //selecciono la colección de la base de datos
+    cart.find({}).toArray((err, result) => { //result es lo que me trae
+        var arrayCart = result[0]; //lo guardo en una variable
+        arrayCart.products.push(request.body.idProduct); //le agrego en texto lo que me llegó de body, le llega en texto
+
+        cart.updateOne({_id: new ObjectID (arrayCart._id) }, //convierte el id qu ele llegó en texto, a un id de mongo
+            {
+                $set: {products: arrayCart.products} //lo actualiza
+            } 
+        );
+        //aseguramos de que no hay error
+        assert.equal(null, err);
+        response.send({
+            message: 'todo bien',
+            arrayCart
+        });
+    });
+});
+
+app.get('/carrito', (request, response) => {
+    const products = db.collection('products');
+    const cart = db.collection("carProdcuts");
+    console.log('Alguien entró al carrito');
+    
+    //buscamos los id de los productos que agregué al carro
+    cart.find({})
+    //transformamos el cursor en un arreglo
+    .toArray((err,result)=>{
+        //aseguramos de que no hay error
+        assert.equal(null, err);
+        
+        var idsCart = [];//un arreglo para guardar todos los ids que tengo en el carrito
+        result[0].products.forEach(id => {
+            idsCart.push(new ObjectID (id));//agrego todos los id al nuevo arreglo
+        });
+        console.log(idsCart);
+    
+        
+        //buscamos todos los productos
+        products.find({ _id: {$in: idsCart}})
+        //transformamos el cursor a un arreglo
+        .toArray((err, resultProducts) => {
+            //aseguramos de que no hay error
+            assert.equal(null, err);
+            var context = {
+                products: resultProducts,
+            };
+            response.render('carProducts',context);
+        });
+    });
+});
 
     app.get('/store/:id', (request, response) => {
 
